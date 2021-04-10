1857ffa2bfa7c3179f93c1918f7aa81d7ea118c3
Marvin GOMES VITORINO <56402327+Gomerinho@users.noreply.github.com>
2021-04-10 17:59:07 +0200

    Ajout des opération et du clear

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+double premierNum;
+bool utilisateurSecondNombre;
+
 #include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -19,9 +22,20 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->pushButton_7, SIGNAL(released()),this,SLOT(digit_pressed()));
     connect(ui->pushButton_8, SIGNAL(released()),this,SLOT(digit_pressed()));
     connect(ui->pushButton_9, SIGNAL(released()),this,SLOT(digit_pressed()));
+
     connect(ui->pushButton_plus_moins, SIGNAL(released()),this,SLOT(operation_pressed()));
     connect(ui->pushButton_pourcent, SIGNAL(released()),this,SLOT(operation_pressed()));
 
+    connect(ui->pushButton_addition, SIGNAL(released()),this,SLOT(operation_binaire_pressed()));
+    connect(ui->pushButton_division, SIGNAL(released()),this,SLOT(operation_binaire_pressed()));
+    connect(ui->pushButton_soustraction, SIGNAL(released()),this,SLOT(operation_binaire_pressed()));
+    connect(ui->pushButton_multiplication, SIGNAL(released()),this,SLOT(operation_binaire_pressed()));
+
+
+    ui->pushButton_addition->setCheckable(true);
+    ui->pushButton_soustraction->setCheckable(true);
+    ui->pushButton_division->setCheckable(true);
+    ui->pushButton_multiplication->setCheckable(true);
 
 }
 
@@ -36,9 +50,22 @@ void MainWindow::digit_pressed()
 
     double nombreLabel;
     QString nouveauLabel;
-    nombreLabel = (ui->label->text() + button->text()).toDouble();
 
-    nouveauLabel = QString::number(nombreLabel, 'g', 15);
+    if ((ui->pushButton_addition->isChecked() || ui->pushButton_soustraction->isChecked() || ui->pushButton_multiplication->isChecked() || ui->pushButton_division->isChecked()) && (!utilisateurSecondNombre)) {
+        nombreLabel = button->text().toDouble();
+        utilisateurSecondNombre = true;
+        nouveauLabel = QString::number(nombreLabel, 'g', 15);
+    }
+    else {
+        if (ui->label->text().contains('.') && button->text()=="0") {
+            nouveauLabel = ui->label->text() + button->text();
+        }else{
+            nombreLabel = (ui->label->text() + button->text()).toDouble();
+            nouveauLabel = QString::number(nombreLabel, 'g', 15);
+        }
+    }
+
+
     ui->label->setText(nouveauLabel);
 }
 
@@ -69,10 +96,58 @@ void MainWindow::operation_pressed()
 
 void MainWindow::on_pushButton_clear_released()
 {
+    ui->pushButton_addition->setChecked(false);
+    ui->pushButton_multiplication->setChecked(false);
+    ui->pushButton_division->setChecked(false);
+    ui->pushButton_soustraction->setChecked(false);
 
+    utilisateurSecondNombre = false;
+
+    ui->label->setText("0");
 }
 
 void MainWindow::on_pushButton_egale_released()
 {
+    double labelNumber, secondNum;
 
+    QString nouveauLabel;
+
+    secondNum = ui->label->text().toDouble();
+
+    if (ui->pushButton_addition->isChecked()) {
+        labelNumber = premierNum + secondNum;
+        nouveauLabel = QString::number(labelNumber, 'g', 15);
+        ui->label->setText(nouveauLabel);
+        ui->pushButton_addition->setChecked(false);
+    }
+    else if (ui->pushButton_soustraction->isChecked()) {
+        labelNumber = premierNum - secondNum;
+        nouveauLabel = QString::number(labelNumber, 'g', 15);
+        ui->label->setText(nouveauLabel);
+        ui->pushButton_soustraction->setChecked(false);
+    }
+    else if (ui->pushButton_multiplication->isChecked()) {
+        labelNumber = premierNum * secondNum;
+        nouveauLabel = QString::number(labelNumber, 'g', 15);
+        ui->label->setText(nouveauLabel);
+        ui->pushButton_multiplication->setChecked(false);
+    }
+    else if (ui->pushButton_division->isChecked()) {
+        labelNumber = premierNum / secondNum;
+        nouveauLabel = QString::number(labelNumber, 'g', 15);
+        ui->label->setText(nouveauLabel);
+        ui->pushButton_division->setChecked(false);
+    }
+
+    utilisateurSecondNombre = false;
 }
+
+void MainWindow::operation_binaire_pressed()
+{
+    QPushButton * button = (QPushButton *) sender();
+
+    premierNum = ui->label->text().toDouble();
+
+    button->setChecked(true);
+}
+
