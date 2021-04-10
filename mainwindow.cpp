2a124dd59a420b3b8afccc3f7795e76527da174e
Marvin GOMES VITORINO <56402327+Gomerinho@users.noreply.github.com>
2021-04-10 17:09:46 +0200

    -Ajout de la connexion des button au label

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->pushButton_0, SIGNAL(released()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_1, SIGNAL(released()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_2, SIGNAL(released()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_3, SIGNAL(released()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_4, SIGNAL(released()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_5, SIGNAL(released()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_6, SIGNAL(released()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_7, SIGNAL(released()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_8, SIGNAL(released()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_9, SIGNAL(released()),this,SLOT(digit_pressed()));
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +26,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::digit_pressed()
+{
+    QPushButton * button = (QPushButton*)sender();
+
+    double nombreLabel;
+    QString nouveauLabel;
+    nombreLabel = (ui->label->text() + button->text()).toDouble();
+
+    nouveauLabel = QString::number(nombreLabel, 'g', 15);
+    ui->label->setText(nouveauLabel);
+}
